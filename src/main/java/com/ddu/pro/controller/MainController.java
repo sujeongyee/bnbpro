b6f12a075fdc1b81429decbcaf980c6b33fc948d
ddu <sujeongyee0529@gmail.com>
2023-08-06 12:44:03 +0900

    컨트롤러수정

--- src/main/java/com/ddu/pro/controller/MainController.java
+++ src/main/java/com/ddu/pro/controller/MainController.java
@@ -1,10 +1,22 @@
 package com.ddu.pro.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.ddu.pro.command.TestVO;
+import com.ddu.pro.service.TestService;
 
 @Controller
 public class MainController {
+	
+	@Autowired
+	TestService testService;
 	
 	@GetMapping("/")
 	public String login() {
@@ -20,6 +32,15 @@ public class MainController {
 	@GetMapping("/main")
 	public String main() {
 		return "main";
+	}
+	@GetMapping("/calendar")
+	public String calendar() {
+		return "calendar";
+	}
+	@GetMapping("/test")
+	public @ResponseBody ResponseEntity<List<com.ddu.pro.command.TestVO>> test(){
+		System.out.println("d");
+		return new ResponseEntity<List<TestVO>>(testService.getTest(), HttpStatus.OK);
 	}
 	//테스트 ddu 로 들어옴
 }
