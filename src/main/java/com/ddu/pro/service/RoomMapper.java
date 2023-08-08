0d9bc98de9e42c415eb534b2cb43102e1654cbf3
mincheol <sken6794@gmail.com>
2023-08-08 14:10:36 +0900

    이미지 업로드 추가

--- src/main/java/com/ddu/pro/service/RoomMapper.java
+++ src/main/java/com/ddu/pro/service/RoomMapper.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import org.apache.ibatis.annotations.Mapper;
 
 import com.ddu.pro.command.LodgmentVO;
+import com.ddu.pro.command.RoomImgVO;
 import com.ddu.pro.command.RoomVO;
 
 @Mapper
@@ -23,4 +24,10 @@ public interface RoomMapper {
 
 	// 사진 조회 할 때만
 	LodgmentVO getLodgment(int lodg_num);
+	
+	// 이미지 등록
+	int registRoomImg(RoomImgVO vo);
+	
+	// 최근 번호 조회
+	ArrayList<RoomVO> getRoomnum();
 }
--- src/main/java/com/ddu/pro/service/RoomServiceImpl.java
+++ src/main/java/com/ddu/pro/service/RoomServiceImpl.java
@@ -1,19 +1,42 @@
 package com.ddu.pro.service;
 
+import java.io.File;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.ddu.pro.command.LodgmentVO;
+import com.ddu.pro.command.RoomImgVO;
 import com.ddu.pro.command.RoomVO;
 
 @Service("roomService")
-public class RoomServiceImpl implements RoomService{
-	
+public class RoomServiceImpl implements RoomService {
+
+	@Value("${project.upload.path}")
+	private String uploadPath;
+
+	// 폴더 생성 함수
+	public String makeFolder() {
+		String path = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyyMMdd"));
+
+		File file = new File(uploadPath + "/" + path);
+		if (file.exists() == false) { // 존재하면 true, 존재하지 않으면 false
+			file.mkdirs();
+		}
+		return path;
+	}
+
 	@Autowired
 	RoomMapper roomMapper;
-	
+
 	@Override
 	public ArrayList<RoomVO> getRoomList(int lodg_num) {
 		return roomMapper.getRoomList(lodg_num);
@@ -32,12 +55,47 @@ public class RoomServiceImpl implements RoomService{
 	@Override
 	public int addRoom(RoomVO vo) {
 		return roomMapper.addRoom(vo);
-				
+
 	}
 
 	@Override
 	public LodgmentVO getLodgment(int lodg_num) {
 		return roomMapper.getLodgment(lodg_num);
+	}
+
+	@Override
+	public int addRoomImg(int roomnum, List<MultipartFile> list) {
+		list = list.stream().filter(p->p.isEmpty()==false).collect(Collectors.toList());
+		for (MultipartFile file : list) {
+
+			String originName = file.getOriginalFilename(); // 파일이름
+
+			String filename = originName.substring(originName.lastIndexOf("\\") + 1); // \\ 기준으로 파일명만 잘라서 저장
+
+			String uuid = UUID.randomUUID().toString(); // 난수 이름
+
+			String filepath = makeFolder(); // 폴더생성
+
+			String savepath = uploadPath + "/" + filepath + "/" + uuid + "_" + filename; // 저장 경로
+
+			try {
+				File saveFile = new File(savepath);
+				file.transferTo(saveFile); // 파일 업로드를 진행
+			} catch (Exception e) {
+				System.out.println("파일업로드 중 error 발생");
+				e.printStackTrace();
+				return 0; // 실패의 의미
+			}
+
+			roomMapper.registRoomImg(RoomImgVO.builder().ro_img_filename(filename).ro_img_filepath(filepath)
+					.ro_img_uuid(uuid).room_num(roomnum).build());
+		}
+		return 1;
+	}
+
+	@Override
+	public ArrayList<RoomVO> getRoomnum() {
+		return roomMapper.getRoomnum();
 	}
 
 }
